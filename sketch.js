12e9e1a0e24f56e0af20f0775d27df9a1d62a66b
aashi2007 <aashi.ranawat@smspune.in>
2020-10-29 17:40:17 +0530

    Vanished pigs

--- sketch.js
+++ sketch.js
@@ -70,8 +70,13 @@ function draw(){
 function mouseDragged(){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
-
-
+function keyPressed()
+{
+    if (keyCode===32)
+    {
+        slingshot.attach(bird.body)
+    }
+}
 function mouseReleased(){
     slingshot.fly();
 }
